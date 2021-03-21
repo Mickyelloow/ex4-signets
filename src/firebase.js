6d9358974cbdddc836be27139853910b78b3dbc9
Mickyelloow <michael.campisi@outlook.com>
2021-03-21 01:54:42 -0400

    - Lien du projet vers mon propre repo

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import 'firebase/firestore';
 
 // Configuration (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyA0KLU036xKZWSdoyg1ad0UbLB9ZDCMsaI",
+  authDomain: "panier-achats-mickyelloow.firebaseapp.com",
+  databaseURL: "https://panier-achats-mickyelloow-default-rtdb.firebaseio.com",
+  projectId: "panier-achats-mickyelloow",
+  storageBucket: "panier-achats-mickyelloow.appspot.com",
+  messagingSenderId: "422473451568",
+  appId: "1:422473451568:web:dbf4200993bf3c293d45f5"
 };
 
 // Initialiser Firebase
